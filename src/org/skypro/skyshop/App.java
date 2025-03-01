00f3ba3069d79862c963a623344d6ae149d2a347
337Nika2004 <borcovaveronika377@gmail.com>
2025-03-01 20:24:39 +0300

    commit 2

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,26 +1,28 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
-import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.*;
 
 public class App {
     public static void main(String[] args) {
         // Создаем несколько продуктов
-        Product product1 = new Product("Яблоки", 100);
-        Product product2 = new Product("Хлеб", 50);
-        Product product3 = new Product("Молоко", 80);
-        Product product4 = new Product("Мясо", 300);
-        Product product5 = new Product("Рыба", 250);
-        Product product6 = new Product("Сок", 120);
+        Product product1 = new SimpleProduct("Яблоки", 100);
+        Product product2 = new SimpleProduct("Хлеб", 50);
+        Product product3 = new DiscountedProduct("Молоко", 80, 20);
+        Product product4 = new DiscountedProduct("Мясо", 300, 10);
+        Product product5 = new FixPriceProduct("Рыба");
+        Product product6 = new FixPriceProduct("Сок");
 
         // Создаем корзину
         ProductBasket basket = new ProductBasket();
+
         // Добавляем продукты в корзину
         basket.addProduct(product1);
         basket.addProduct(product2);
         basket.addProduct(product3);
         basket.addProduct(product4);
         basket.addProduct(product5);
+
         // Попытка добавить продукт в заполненную корзину
         basket.addProduct(product6);
 
