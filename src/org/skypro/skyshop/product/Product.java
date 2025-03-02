376d981844c8250f1ba08c60146971c7d185ca9e
337Nika2004 <borcovaveronika377@gmail.com>
2025-03-02 15:47:44 +0300

    4

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,6 +1,8 @@
 package org.skypro.skyshop.product;
 
-public abstract class Product {
+import org.skypro.skyshop.search.Searchable;
+
+public abstract class Product implements Searchable {
     private final String name;
 
     public Product(String name) {
@@ -15,6 +17,16 @@ public abstract class Product {
 
     public boolean isSpecial() {
         return false;
+    }
+
+    @Override
+    public String getSearchTerm() {
+        return name;
+    }
+
+    @Override
+    public String getContentType() {
+        return "PRODUCT";
     }
 
     @Override
